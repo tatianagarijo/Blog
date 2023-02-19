49064d3d8d5114a81efaecd972a55018fb4fe069
TATIANA <TATIANA@DESKTOP-CO9O96Q>
2023-02-19 16:03:19 +0100

    Mejorando

--- src/EntradaBlogTGA.java
+++ src/EntradaBlogTGA.java
@@ -1,11 +1,13 @@
 /**
  * Clase para manejar las entradas de un blog.
- * La clase se creó @since 19/02/23 por @author: Tatiana Garijo Amorós.
- * Esta es la @version: 2.4
+ * 
+ * @since 19/02/23 
+ * @author Tatiana Garijo Amorós.
+ * @version 2.4
 */
 public class EntradaBlogTGA {
     /**
-     * la variable separador se usa para dar formato a la cadena de texto y separar el título del autor
+     * La variable separador se usa para dar formato a la cadena de texto y separar el título del autor
      */
 
     public static char separador=':';
@@ -14,13 +16,13 @@ public class EntradaBlogTGA {
     private final String autor;
 
     /**
-     * Constructor de la clase
-     * Recibe los parámetros:
+     * Constructor de la clase.
+     * Recibe:
      * @param id número de entrada del Blog
      * @param autor nombre del autor del Blog
      * @param texto texto de la entrada del Blog
      *
-     * Si el id (número de entrada) es null, negativo o igual a 0, lanza una excepción @trows IllegalArgumentException
+     * @throws IllegalArgumentException se lanza si el id (número de entrada) es null, negativo o igual a 0
      */
     public EntradaBlogTGA(int id, String autor, String texto)throws IllegalArgumentException{
         if(id<=0) throw new IllegalArgumentException("El id no puede ser negativo");
@@ -30,8 +32,8 @@ public class EntradaBlogTGA {
     }
 
     /**
-     *
-     * @return cad la cadena de texto se haformado y se le ha dado formato a los datos de la entrada del Blog, autor y texto.
+     *Devuelve una cadena creada para dar formato a la salida de los valores.
+     * @return cad 
      */
     @Override
     public String toString(){
